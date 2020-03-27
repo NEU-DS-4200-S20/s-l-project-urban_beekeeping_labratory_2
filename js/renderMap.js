a7f5da19bdc71bb53ccd4988375663c4c85c59ea
martinmat98 <martin.mat@husky.neu.edu>
2020-03-27 17:28:03 -0400

    updated map

--- js/renderMap.js
+++ js/renderMap.js
@@ -9,29 +9,21 @@ var path = d3.geoPath().projection(projection);
 var svg = d3.select("#map-container").append("svg")
   .attr("width", '100%')
   .attr("height", height)
-  .attr('position', 'relative')
   .append("g")
-  .attr("transform", "translate(-2100,-100)scale(3)");
+  .attr("transform", "translate(-2100, -200)scale(3)");
 
 var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 queue()
-  .defer(d3.json, "../data/ma.topo.json")
-  .defer(d3.json, "../data/ct.topo.json")
-  .defer(d3.json, "../data/nh.topo.json")
-  .defer(d3.json, "../data/vt.topo.json")
-  .defer(d3.json, "../data/ri.topo.json")
+  .defer(d3.json, "../data/ma.topojson")
   .await(ready);
 
-function ready(error, ma, ct, nh, vt, ri, me) {
+function ready(error, ma) {
 
   var matopo = { type: "FeatureCollection", id: "MA", features: topojson.feature(ma, ma.objects.ma).features };
-  var cttopo = { type: "FeatureCollection", id: "CT", features: topojson.feature(ct, ct.objects.ct).features };
-  var nhtopo = { type: "FeatureCollection", id: "NH", features: topojson.feature(nh, nh.objects.nh).features };
-  var vttopo = { type: "FeatureCollection", id: "VT", features: topojson.feature(vt, vt.objects.vt).features };
-  var ritopo = { type: "FeatureCollection", id: "RI", features: topojson.feature(ri, ri.objects.ri).features };
+  console.log(matopo['features'])
 
-  var topo = [matopo, cttopo, nhtopo, vttopo, ritopo];
+  var topo = [matopo];
 
   var town = svg.selectAll(".land").data(topo);
 
@@ -40,6 +32,7 @@ function ready(error, ma, ct, nh, vt, ri, me) {
     .attr("d", path)
     .attr("id", function (d, i) { return d.id; })
     .style("fill", function (d, i) { return "#0000" });
+
 
 
   //tooltips
